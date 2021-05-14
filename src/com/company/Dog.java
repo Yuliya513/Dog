7b2f0cb2cc7bce10ecf45cbaf89dac30eb3847c4
Yuliya513 <yuliya.senik@gmail.com>
2021-05-14 14:55:55 +0300

    break,switch

---
+++ src/com/company/Dog.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Dog {
+
+    public static void main(String[] args) {
+	private static String name Dog=I;
+	private static int age=11;
+        private static String breed=1;
+        public static void main(String[] args){
+            int age=5;
+            switch (animal){
+                case dog
+                System.out.println ("Is it a dog");
+                break;
+                System.out.println ("Is it a breed");
+                break;
+                default
+                    System.out.println ("Age");
+
+            }
+        }
+    }
+}
